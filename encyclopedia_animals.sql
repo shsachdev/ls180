dde78be2ad885c8c96d6fdce27a45f24ededf462
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-09 19:58:56 -0800

    adding tables to encyclopedia

--- encyclopedia_animals.sql
+++ encyclopedia_animals.sql
@@ -1,4 +1,8 @@
 CREATE TABLE animals (
-    name text,
-    
+    id serial,
+    name varchar(100) NOT NULL,
+    binomial_name varchar(100) NOT NULL,
+    max_weight_kg decimal(8,3),
+    max_age_years integer,
+    conservation_status char(2)
 );
