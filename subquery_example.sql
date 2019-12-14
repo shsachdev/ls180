17bb97a486f511d85a8c9231fd77397a22d73fde
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-13 20:38:48 -0800

    example subqueries

---
+++ subquery_example.sql
@@ -0,0 +1,8 @@
+SELECT u.full_name FROM users u
+WHERE u.id NOT IN (SELECT c.user_id FROM checkouts c);
+
+-- Above can be done using Join statements
+
+SELECT u.full_name FROM users u
+LEFT JOIN checkouts c ON (u.id = c.user_id)
+WHERE c.user_id IS NULL;
